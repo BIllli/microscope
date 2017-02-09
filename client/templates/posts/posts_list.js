6b255434fb41d2573bac6ff41abaeedca9ba3289
BIllli <power_free@126.com>
2017-02-09 02:51:36 +0000

    first commit

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,24 @@
+var postsData=[
+{
+    title:'Introducing Telescope',
+    url:'http://sachagreif.com/introducing-telescope'
+},
+{
+    title:'meteor',
+    url:'http://meteor.com'
+},
+{
+    title:'Baidu',
+    url:'http://baidu.com'
+}
+
+];
+
+
+Template.postsList.helpers(
+{posts:postsData}
+);
+
+
+
+
